723e8a5bd9f6f8400ecd5f71d8f03f1179842ebc
Fellipe Couto <fellipe@vulpi.com.br>
2021-05-03 11:56:03 -0300

    Criando autenticação via API do Google Sheets

---
+++ read_csv.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+
+#Credenciais para acessar a planilha
+SHEET_SALARY_ID = '1x6CROvObenzmRbypEm2cch4PvWqKrHV3wGa6BbL9G6I'
+CREDENTIALS_JSON = 'puc-sheets-fb4da8a3aee2.json'
+
+#Escopo utilizado
+scope = ['https://spreadsheets.google.com/feeds']
+
+#Autendicando
+credentials = ServiceAccountCredentials.from_json_keyfile_name(CREDENTIALS_JSON, scope)
+gc = gspread.authorize(credentials)
+wks = gc.open_by_key(SHEET_SALARY_ID)
+
+#Seleciona a primeira página da planilha
+worksheet = wks.get_worksheet(0)
